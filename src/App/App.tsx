f4f4beb7e64b529a7994ce9b4ce5e6e6bdecebc6
RTSaitama <rtsaitama@outlook.com>
2025-05-31 23:28:00 +0300

    added itunes fetch for data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,36 +1,78 @@
-import { useState } from "react";
-
-import '../styles/main.scss'
+import { useState, useEffect } from "react";
+import '../styles/main.scss';
 import { SideMenu } from "../components/SideMenu/SideMenu";
 import { MusicScreen } from "../components/MusicScreen/MusicScreen";
 import { Hero } from "../components/Hero/Hero";
 import { Footer } from "../components/Footer/Footer";
 
-const App: React.FC = () => {
-  const [menuMobile, setMenuMobile] = useState<boolean>(false);
+export interface Track {
+  id: string;
+  title: string;
+  artist: string;
+  image: string;
+}
 
+export interface Album {
+  id: string;
+  title: string;
+  artist: string;
+  image: string;
+}
+const App: React.FC = () => {
+  const [menuMobile, setMenuMobile] = useState(false);
+  const [latestTracks, setLatestTracks] = useState<Track[]>([]);
+  const [latestAlbums, setLatestAlbums] = useState<Track[]>([]);
+  
+  useEffect(() => {
+    fetch('https://itunes.apple.com/us/rss/topsongs/limit=10/json')
+      .then(response => response.json())
+      .then(data => {
+        const entries = data.feed.entry;
+        const parsTracks: Track[] = entries.map((entry: any) => ({
+          id: entry.id.attributes['im:id'],
+          title: entry['im:name'].label,
+          artist: entry['im:artist'].label,
+          image: entry['im:image'][2].label,
+        }));
+        setLatestTracks(parsTracks);
+      })
+      .catch(error => console.error('Помилка:', error));
+  }, []);
+  useEffect(() => {
+    fetch('https://itunes.apple.com/us/rss/topalbums/limit=10/json')
+      .then(response => response.json())
+      .then(data => {
+        const entries = data.feed.entry;
+        const parsAlbums: Album[] = entries.map((entry: any) => ({
+          id: entry.id.attributes['im:id'],
+          title: entry['im:name'].label,
+          artist: entry['im:artist'].label,
+          image: entry['im:image'][2].label,
+        }));
+        setLatestAlbums(parsAlbums);
+      })
+      .catch(error => console.error('Помилка:', error));
+  }, []);
   return (
     <div className="App page__wrapper">
       <main className="main">
         <button
-          className="open__menu__btn "
+          className="open__menu__btn"
           onClick={() => setMenuMobile(prev => !prev)}
         >
           <svg className="burger__svg">
-            {" "}
             <use href="/icons/sprite.svg#burger__icon" />
           </svg>
         </button>
         <SideMenu mobile={menuMobile} />
         <div className="main__container container">
           <Hero />
-          <MusicScreen />
+          <MusicScreen tracks={latestTracks} albums={latestAlbums}/>
         </div>
       </main>
       <Footer />
-
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MusicScreen/MusicScreen.tsx
+++ src/components/MusicScreen/MusicScreen.tsx
@@ -1,242 +1,73 @@
 import React from 'react'
 import { Swiper } from '../Swiper/Swiper'
+import { Track } from '../../App/App'
+import { Album } from '../../App/App'
 
-interface Props{ 
-
+interface Props {
+  tracks: Track[];
+  albums: Album[];
 }
 
-export const MusicScreen: React.FC<Props> = () => {
+
+export const MusicScreen: React.FC<Props> = ({ tracks, albums}) => {
+
+
+
+
   return (
     <div className="music__screen container">
-      <Swiper/>
-  <div className="latest__music container music__screen__part">
-    <div className="latest__album latest__music__part">
-      <h3 className="latest__heading">latest album</h3>
-      <ul className="latest__list">
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-      </ul>
+      <Swiper />5+  
+      <div className="latest__music container music__screen__part">
+        <div className="latest__album latest__music__part">
+          <h3 className="latest__heading">latest album</h3>
+          <ul className="latest__list">
+           {albums?.length > 0 ? (
+              albums.map(album => (
+                <li className="latest__li" key={album.id}>
+                  <div className="track__wrapp">
+                    <img
+                      className="track__img"
+                      src={album.image}
+                      alt="track__img"
+                    />
+                    <div className="track_info">
+                      <p className="track__name">{album.title}</p>
+                      <p className="track__artist">{album.artist}</p>
+                    </div>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p>Немає треків</p>
+            )}
+          </ul>
+        </div>
+        <div className="latest__singles latest__music__part">
+          <h3 className="latest__heading">latest sings </h3>
+          <ul className="latest__list">
+         {tracks?.length > 0 ? (
+              tracks.map(track => (
+                <li className="latest__li" key={track.id}>
+                  <div className="track__wrapp">
+                    <img
+                      className="track__img"
+                      src={track.image}
+                      alt="track__img"
+                    />
+                    <div className="track_info">
+                      <p className="track__name">{track.title}</p>
+                      <p className="track__artist">{track.artist}</p>
+                    </div>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p>Немає треків</p>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
-    <div className="latest__singles latest__music__part">
-      <h3 className="latest__heading">latest sings </h3>
-      <ul className="latest__list">
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-        {"{"}" "{"}"}
-        <li className="latest__li">
-          <div className="track__wrapp">
-            <img
-              className="track__img"
-              src="./muzplayer/src/assets/images/latest demo look/gorillaz track picture.jpg"
-              alt="track__img"
-            />
-            <div className="track_info">
-              <p className="track__name">Feel Good Inc.</p>
-              <p className="track__artist">Gorillaz</p>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
 
   )
 }
